881da459a5f8bd4578b2274d5392502d1757e354
maheshmeenabalot <mahesh.morandi@gmail.com>
2024-04-23 14:09:57 +0530

    Fixed duplicate file issue

--- Frontend/Genral.js
+++ Frontend/Genral.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var HomeBottons = document.querySelector('.HomeBottons');
     var loginContainer = document.querySelector('.loginContainer');
     var userDetailsContainer = document.getElementById('userDetailsContainer');
+    var aboutUs = document.querySelector('#aboutUs');
+    var viewCourse = document.querySelector('#instructor-Courses'); 
     //addstu
     var addStudeBtnCont = document.getElementById('addStudeBtn');
 
@@ -52,6 +54,8 @@ document.addEventListener('DOMContentLoaded', function () {
         showSearchContainer();
         filterBtn.style.display = 'none';
         enrolledStudents.style.display = 'none';
+        instructorList.style.display = 'none';
+        document.getElementById('instructorCourseList').style.display = 'none';
         hideLoginContainer();
         hideHomeBottons();
         
@@ -61,8 +65,11 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         showSearchContainer();
         //addstu
-        showaddStudeBtnCont();
+        // showaddStudeBtnCont();
+        document.getElementById('studentData').style.display='block';
+        document.getElementById('instructorCourseList').style.display = 'none'
         addCourseBtn.style.display = 'none';
+        instructorList.style.display = 'none';
         hideLoginContainer();
         hideHomeBottons();
         
@@ -73,7 +80,24 @@ document.addEventListener('DOMContentLoaded', function () {
         hideContainers();
         showLoginContainer();
         showHomeBottons();
+        document.getElementById('instructorCourseList').style.display = 'none'
         
+    });
+
+    InstList.addEventListener('click', function (event) {
+        event.preventDefault();
+        showSearchContainer();
+        addCourseBtn.style.display = 'none';
+        instructorList.style.display = 'block';
+        hideLoginContainer();
+        hideHomeBottons();
+        document.getElementById('instructorCourseList').style.display = 'none'
+        // addStudeBtn.style.display = 'none';
+        // addCourseBtn.style.display = 'none';
+        // FilterBtn.style.display = 'none';
+        // addInstructorBtn.style.display = 'block';
+        // // searchInput.value = '';
+
     });
 
     queryLink.addEventListener('click', function (event) {
@@ -81,9 +105,35 @@ document.addEventListener('DOMContentLoaded', function () {
         showQueryContainer();
         hideLoginContainer();
         hideHomeBottons();
+        instructorList.style.display = 'none';
+        document.getElementById('instructorCourseList').style.display = 'none'
        
     });
 
+    aboutUs.addEventListener('click', function (event) {
+        event.preventDefault();
+        hideLoginContainer();
+        hideContainers() ;
+        document.getElementById('instructorList').style.display='none';
+        document.getElementById('enrolledStudents').style.display='none';
+        document.getElementById('studentData').style.display='none';
+        document.getElementById('personalDetailsDropdown').style.display='none';
+        document.getElementById('academicDetailsDropdown').style.display='none';  
+        document.getElementById('instructorCourseList').style.display = 'none'  
+       
+    });
+    viewCourse.addEventListener('click', function (event) {
+        event.preventDefault();
+        hideLoginContainer();
+        hideContainers() ;
+        document.getElementById('instructorList').style.display='none';
+        document.getElementById('enrolledStudents').style.display='none';
+        document.getElementById('studentData').style.display='none';
+        document.getElementById('personalDetailsDropdown').style.display='none';
+        document.getElementById('academicDetailsDropdown').style.display='none';   
+        document.getElementById('instructorCourseList').style.display = 'block' 
+       
+    });
     // Function to show search container
     function showSearchContainer() {
         searchContainer.style.display = 'block';
@@ -113,7 +163,9 @@ document.addEventListener('DOMContentLoaded', function () {
         addstudentForm.style.display = 'none';
         addCourseForm.style.display = 'none';
         filterForm.style.display = 'none';
-
+        
+        
+        // document.getElementById('studentData').style.display='none';
     }
     
 });
@@ -148,6 +200,7 @@ document.getElementById("homeLink").addEventListener("click", function () {
     // Show the personal details dropdown
     document.getElementById("personalDetailsDropdown").style.display = "block";
     document.getElementById('enrolledStudents').style.display = "none";
+    document.getElementById('instructorList').style.display = "none";
 });
 
 
